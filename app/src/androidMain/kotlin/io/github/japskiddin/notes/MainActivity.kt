2d9d8fd3bb91dd9191a4e619affbfa22797a8192
Nikita <japskiddin@gmail.com>
2025-03-20 19:12:46 +0800

    - Enable edge-to-edge in android activity

--- app/src/androidMain/kotlin/io/github/japskiddin/notes/MainActivity.kt
+++ app/src/androidMain/kotlin/io/github/japskiddin/notes/MainActivity.kt
@@ -3,6 +3,7 @@ package io.github.japskiddin.notes
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import com.arkivanov.decompose.defaultComponentContext
 import io.github.japskiddin.notes.component.RootComponent
 import org.koin.android.ext.android.inject
@@ -10,6 +11,7 @@ import org.koin.android.ext.android.inject
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         val rootComponentFactory: RootComponent.Factory by inject()
         val rootComponent = rootComponentFactory(defaultComponentContext())
